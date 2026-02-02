bb2b65f242a0a94e199b861149ebc805c770300b
pradhuman <pradhumantamboli12@gmail.com>
2026-02-02 15:37:18 +0530

    add start server script for windows.

---
+++ start_server.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Go to project folder
+cd /d C:\Users\Shree\Desktop\MTC\ecommarce_website\ecommarce_website
+
+REM Activate virtual environment
+call venv\Scripts\activate.bat
+
+REM Optional: check python path
+where python
+
+REM Start Django server
+python manage.py runserver 0.0.0.0:8000
+
+pause
